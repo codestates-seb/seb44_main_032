afcd7c0c47276587394f30135be7474177b1ac8f
gibbeunLee <124775054+gibbeunLee@users.noreply.github.com>
2023-06-30 16:10:10 +0900

    setup : default file setup at dev branch

---
+++ server/src/main/java/com/codeassembly/CodeAssemblyApplication.java
@@ -0,0 +1,13 @@
+package com.codeassembly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeAssemblyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeAssemblyApplication.class, args);
+	}
+
+}
