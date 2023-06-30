c1181f245128c69b54e9f839aa2243412f51bd1f
gibbeunLee <124775054+gibbeunLee@users.noreply.github.com>
2023-06-30 16:07:10 +0900

    setup : default file setup at dev branch

---
+++ server/src/main/java/com/codeassembly/response/PageInfo.java
@@ -0,0 +1,13 @@
+package com.codeassembly.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class PageInfo {
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+}
