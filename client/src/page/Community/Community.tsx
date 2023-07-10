5ac5dde0e400463a898d58a4ed4ea268b2fc2441
Hye In Shin <hyein0608sa@gmail.com>
2023-07-10 13:07:43 +0900

    feat : community 카드 만들기

--- client/src/page/Community/Community.tsx
+++ client/src/page/Community/Community.tsx
@@ -1,5 +1,17 @@
+import styled from 'styled-components';
+
+const PostsCard = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 904px;
+  height: 128px;
+  border: 1px solid black;
+  border-radius: 8px;
+`;
+
 function Community() {
-  return <div>Community</div>;
+  return <PostsCard></PostsCard>;
 }
 
 export default Community;
