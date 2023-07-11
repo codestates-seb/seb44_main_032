0604b32b435d852c36c318d81a8cb7a7fd4e56db
Yeeri <ye230212@gmail.com>
2023-07-12 02:50:08 +0900

    feat.plan page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
-
+import { useState } from 'react';
 import Login from "./page/Login/Login"
 import SignUp from "./page/SignUp/SignUp"
+import Plan from "./page/Plan/Plan"
+import Edit from "./page/Plan/Edit"
+
 
 function App() {
+  const [planData, setPlanData] = useState<{ title: string; date: string; content: string } | null>(null);
+
 
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/plan" element={<Plan planData={planData} />} />
+      <Route path="/plan/post" element={<Edit setPlanData={setPlanData} />} />
+   
     </Routes>
   )
 }
 
-export default App
+export default App;
