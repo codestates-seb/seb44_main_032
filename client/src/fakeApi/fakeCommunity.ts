ba8908b10124325b57597d7f6b2d6f3ccd1c9690
Hye In Shin <hyein0608sa@gmail.com>
2023-07-14 10:38:19 +0900

    feat : 커뮤니티 api 작업

--- client/src/fakeApi/fakeCommunity.ts
+++ client/src/fakeApi/fakeCommunity.ts
@@ -1,8 +1,11 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export default class FakeCommunity {
     constructor() {}
     async getCommunityList() {
         return axios.get('/community/getCommunityList.json').then((res) => res.data)
+    }
+    async getCommunitySearch() {
+        return axios.get('/community/search.json').then((res) => res.data)
     }
 }
