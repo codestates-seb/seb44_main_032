c1be25992a4af7a49fa8c6de582ad83b275545bc
YoonE_ <wldbseja@daum.net>
2023-07-14 09:50:31 +0900

    feat : 1차 기능구현 커밋

--- client/src/page/Community/CommunityDetail.tsx
+++ client/src/page/Community/CommunityDetail.tsx
@@ -1,5 +1,127 @@
+import { useState, useEffect } from 'react';
+import { useParams, Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+type CommunityPost = {
+  communityId: number;
+  title: string;
+  content: string;
+  writer: {
+    memberId: number;
+    nickname: string;
+  };
+  category: string; // 카테고리 필드 추가
+};
+
 function CommunityDetail() {
-  return <div>Community Detail</div>;
+  const { communityId } = useParams<{ communityId: string }>();
+  const [post, setPost] = useState<CommunityPost | null>(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
+  async function fetchPost() {
+    try {
+      // 더미 데이터로 대체
+      const communityPost: CommunityPost = {
+        communityId: 1,
+        title: '더미 데이터 제목',
+        content: '더미 데이터 내용',
+        writer: {
+          memberId: 1,
+          nickname: 'hgd1',
+        },
+        category: '카테고리', // 카테고리 설정
+      };
+
+      setPost(communityPost);
+      // 실제 API 호출 코드
+      // const response = await axios.get<CommunityPost>(`/community/${communityId}`);
+      // setPost(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  function handleEditButtonClick() {
+    navigate(`/community/${communityId}/edit`, { state: { post } });
+  }
+
+  if (!post) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>커뮤니티 상세 페이지</h1>
+      <h2>{post.title}</h2>
+      <p>{post.content}</p>
+      <p>작성자: {post.writer.nickname}</p>
+      <p>카테고리: {post.category}</p> {/* 카테고리 표시 */}
+      <button onClick={handleEditButtonClick}>수정</button>
+      <Link to="/community">목록으로</Link>
+    </div>
+  );
 }
 
 export default CommunityDetail;
+
+// const DetailSection = styled.div`
+//   display: flex;
+//   height: 100%;
+//   justify-content: center;
+//   align-items: center;
+//   background-color: #f9f9f9;
+// `;
+
+// const EditSection = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   margin-top: auto;
+//   height: 607px;
+//   width: 960px;
+//   border-radius: 20px;
+//   padding: 16px;
+//   border: 1px solid #98dde3;
+//   background-color: white;
+// `;
+
+// const TitleText = styled.div`
+//   font-size: 20px;
+//   font-weight: bold;
+// `;
+
+// const ContentText = styled.div`
+//   font-size: 20px;
+//   font-weight: bold;
+// `;
+
+// const ButtonWrapper = styled.div`
+//   display: flex;
+//   justify-content: flex-end;
+//   margin-top: 24px;
+//   height: 37px;
+// `;
+
+// const EditButton = styled.button`
+//   padding: 6px;
+//   border-radius: 8px;
+//   border: 0px;
+//   width: 69px;
+//   background-color: #98dde3;
+//   color: white;
+//   cursor: pointer;
+//   margin-left: 12px;
+// `;
+
+// const DeleteButton = styled.button`
+//   padding: 6px;
+//   border-radius: 8px;
+//   border: 0px;
+//   width: 69px;
+//   background-color: #eea9a9;
+//   color: white;
+//   cursor: pointer;
+// `;
--- client/src/page/Community/CommunityEdit.tsx
+++ client/src/page/Community/CommunityEdit.tsx
@@ -1,5 +1,71 @@
+import { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+type CommunityPost = {
+  communityId: number;
+  title: string;
+  content: string;
+  writer: {
+    memberId: number;
+    nickname: string;
+  };
+  category: string; // 카테고리 필드 추가
+};
+
 function CommunityEdit() {
-  return <div>Community Edit</div>;
+  const location = useLocation<{ post: CommunityPost }>();
+  const navigate = useNavigate();
+  const [editedPost, setEditedPost] = useState<CommunityPost | null>(null);
+
+  useEffect(() => {
+    if (location.state) {
+      setEditedPost(location.state.post);
+    }
+  }, [location.state]);
+
+  function handleInputChange(
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) {
+    const { name, value } = e.target;
+    setEditedPost(prevPost => ({
+      ...prevPost!,
+      [name]: value,
+    }));
+  }
+
+  function handleSaveButtonClick() {
+    console.log('Edited Post:', editedPost);
+    navigate(`/community/${editedPost?.communityId}`);
+  }
+
+  if (!editedPost) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>커뮤니티 게시글 수정</h1>
+      <input
+        type="text"
+        name="title"
+        value={editedPost.title}
+        onChange={handleInputChange}
+      />
+      <textarea
+        name="content"
+        value={editedPost.content}
+        onChange={handleInputChange}
+      ></textarea>
+      <input
+        type="text"
+        name="category"
+        value={editedPost.category}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleSaveButtonClick}>저장</button>
+      <button>취소</button>
+    </div>
+  );
 }
 
 export default CommunityEdit;
