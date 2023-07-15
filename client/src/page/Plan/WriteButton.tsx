d950229e9ff1a88a1141ceb26bc02c28cfef7ff3
YoonE_ <wldbseja@daum.net>
2023-07-15 15:15:21 +0900

    Update WriteButton.tsx

--- client/src/page/Plan/WriteButton.tsx
+++ client/src/page/Plan/WriteButton.tsx
@@ -1,17 +1,56 @@
-import styled from "styled-components";
-import { PiPencilSimple } from "react-icons/pi";
-import { Link } from "react-router-dom";
+// import styled from "styled-components";
+// import { PiPencilSimple } from "react-icons/pi";
+// import { Link } from "react-router-dom";
 
 
-// const WriteButtonBtn = styled(Link)`
+// // const WriteButtonBtn = styled(Link)`
+// //   position: sticky;
+// //   top: 130px; /* 원하는 위치에 맞게 조절 */
+// //   /* right: 20px; */
+// //   z-index: 999; //다른 요소 위에 나타나도록 설정
+// //   /* float: right; */
+// //   display: flex;
+// //   align-items: center;
+// //   justify-content: center;
+// //   width: 46px;
+// //   height: 44px;
+// //   background: #98DDE3;
+// //   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+// //   border-radius: 25px;
+// //   border: none;
+// //   color: white;
+
+// //   @media (min-width: 767px) {
+// //     margin-left: 196px;
+// //     top: 20px; /* 화면 오른쪽 상단에 고정될 위치로 수정 */
+// //   }
+  
+// // `;
+
+// // function WriteButton() {
+// //   return (
+// //     <WriteButtonBtn to="/edit">
+// //       <PiPencilSimple />
+// //     </WriteButtonBtn>
+// //   );
+// // }
+
+// // export default WriteButton;
+
+// const WriteButtonContainer = styled.div`
 //   position: sticky;
-//   top: 130px; /* 원하는 위치에 맞게 조절 */
-//   /* right: 20px; */
-//   z-index: 999; //다른 요소 위에 나타나도록 설정
-//   /* float: right; */
+//   top: 20px; /* 원하는 위치에 맞게 조절 */
+//   right: 20px; /*  */
+//   z-index: 999; // 다른 요소 위에 나타나도록 설정
 //   display: flex;
 //   align-items: center;
 //   justify-content: center;
+//   margin-left: auto; /* 오른쪽으로 이동 */
+//   border: 1px solid gray; //임시
+
+// `;
+
+// const WriteButtonBtn = styled(Link)`
 //   width: 46px;
 //   height: 44px;
 //   background: #98DDE3;
@@ -19,55 +58,16 @@ import { Link } from "react-router-dom";
 //   border-radius: 25px;
 //   border: none;
 //   color: white;
-
-//   @media (min-width: 767px) {
-//     margin-left: 196px;
-//     top: 20px; /* 화면 오른쪽 상단에 고정될 위치로 수정 */
-//   }
-  
 // `;
 
 // function WriteButton() {
 //   return (
-//     <WriteButtonBtn to="/edit">
-//       <PiPencilSimple />
-//     </WriteButtonBtn>
+//     <WriteButtonContainer>
+//       <WriteButtonBtn to="/edit">
+//         <PiPencilSimple />
+//       </WriteButtonBtn>
+//     </WriteButtonContainer>
 //   );
 // }
 
 // export default WriteButton;
-
-const WriteButtonContainer = styled.div`
-  position: sticky;
-  top: 20px; /* 원하는 위치에 맞게 조절 */
-  right: 20px; /*  */
-  z-index: 999; // 다른 요소 위에 나타나도록 설정
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-left: auto; /* 오른쪽으로 이동 */
-  border: 1px solid gray; //임시
-
-`;
-
-const WriteButtonBtn = styled(Link)`
-  width: 46px;
-  height: 44px;
-  background: #98DDE3;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 25px;
-  border: none;
-  color: white;
-`;
-
-function WriteButton() {
-  return (
-    <WriteButtonContainer>
-      <WriteButtonBtn to="/edit">
-        <PiPencilSimple />
-      </WriteButtonBtn>
-    </WriteButtonContainer>
-  );
-}
-
-export default WriteButton;
