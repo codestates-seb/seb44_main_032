32b2737d3f0544fdd35fed337fa19c943e003673
YoonE_ <wldbseja@daum.net>
2023-07-18 01:57:43 +0900

    feat : 포스트 수정, 등록 기능 완료

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,8 +5,7 @@ import SignUp from './page/SignUp/SignUp';
 import Header from './components/Header/Header';
 import Community from './page/Community/Community';
 import CommunityDetail from './page/Community/CommunityDetail';
-import CommunityEdit from './page/Community/CommunityEdit';
-import CommunityPost from './page/Community/CommunityPost';
+import CommunityPostForm from './page/Community/CommunityPostForm';
 import Main from './page/Main/Main';
 import Plan from './page/Plan/Plan';
 import Edit from './page/Plan/Edit';
@@ -27,8 +26,8 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/community" element={<Community />} />
         <Route path="/community/:id" element={<CommunityDetail />} />
-        <Route path="/community/:id/edit" element={<CommunityEdit />} />
-        <Route path="/community/post" element={<CommunityPost />} />
+        <Route path="/community/:id/edit" element={<CommunityPostForm />} />
+        <Route path="/community/post" element={<CommunityPostForm />} />
         <Route path="/plan" element={<Plan planData={planData} />} />
         <Route path="/plan/post" element={<Edit setPlanData={setPlanData} />} />
       </Routes>
