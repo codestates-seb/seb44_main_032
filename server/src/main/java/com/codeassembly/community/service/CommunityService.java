5472731df7007631a561bbcf28233ea3484805b7
gibbeunLee <124775054+gibbeunLee@users.noreply.github.com>
2023-07-20 13:42:35 +0900

    fix : applycation.yml

--- server/src/main/java/com/codeassembly/community/service/CommunityService.java
+++ server/src/main/java/com/codeassembly/community/service/CommunityService.java
@@ -94,7 +94,7 @@ public class CommunityService {
         Community community = communityRepository.findById(communityId).orElseThrow(() -> new BusinessLogicException(ExceptionCode.COMMUNITY_NOT_FOUND));
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user = userRepository.findByName(authentication.getName()).orElseThrow(() -> new BusinessLogicException(ExceptionCode.USER_NOT_FOUND));
+        User user = userRepository.findByEmail(authentication.getName()).orElseThrow(() -> new BusinessLogicException(ExceptionCode.USER_NOT_FOUND));
 
         if (likeCommunityRepository.findByUserAndCommunity(user, community) == null) {
             // 좋아요를 누른적 없다면 LikeCommunity 생성 후, 좋아요 처리
@@ -118,5 +118,3 @@ public class CommunityService {
     }
 
 }
-
-
--- server/src/main/java/com/codeassembly/plan/controller/PlanController.java
+++ server/src/main/java/com/codeassembly/plan/controller/PlanController.java
@@ -6,7 +6,12 @@ import com.codeassembly.plan.dto.PlanDto;
 import com.codeassembly.plan.entity.Plan;
 import com.codeassembly.plan.mapper.PlanMapper;
 import com.codeassembly.plan.service.PlanService;
+import com.codeassembly.response.MultiResponseDto;
+import java.util.List;
 import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import org.joda.time.LocalDate;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -17,6 +22,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -52,15 +58,15 @@ public class PlanController {
     @PatchMapping("/{templatateId}/edit")
     public ResponseEntity updatePlan(@PathVariable("templatateId") Long templatateId,
                                      @Valid @RequestBody PlanDto.Patch requestBody) {
-       Plan plan = mapper.planPatchToPlan(requestBody);
-       Plan updatePlan = planService.updatePlan(templatateId, plan);
-       return new ResponseEntity<>(mapper.planToPlanResponse(updatePlan), HttpStatus.OK);
+        Plan plan = mapper.planPatchToPlan(requestBody);
+        Plan updatePlan = planService.updatePlan(templatateId, plan);
+        return new ResponseEntity<>(mapper.planToPlanResponse(updatePlan), HttpStatus.OK);
     }
 
     @DeleteMapping("/{templatateId}")
     public ResponseEntity deletePlan(@PathVariable("templatateId") Long templatateId){
         planService.deletePlan(templatateId);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/detail/{templatateId}")
@@ -69,15 +75,15 @@ public class PlanController {
         return new ResponseEntity<>(mapper.planToPlanResponse(plan), HttpStatus.OK);
     }
 
-    
+
     @GetMapping("/category/{category}")
     public ResponseEntity getPlansByCategoryAndPage(
-        @PathVariable("category") String category,
-        @Positive @RequestParam(defaultValue = "1") int page,
-        @Positive @RequestParam(defaultValue = "10") int size) {
+            @PathVariable("category") String category,
+            @Positive @RequestParam(defaultValue = "1") int page,
+            @Positive @RequestParam(defaultValue = "10") int size) {
 
         Page<Plan> pagePlan = planService.findPlanByCategory(category, page - 1, size);
-        List<Plan> communities = pagePlan.getContent();
-        return new ResponseEntity<>(new MultiResponseDto<>(mapper.plansToPlanResponses(communities), pagePlan), HttpStatus.OK);
+        List<Plan> plans = pagePlan.getContent();
+        return new ResponseEntity<>(new MultiResponseDto<>(mapper.plansToPlanResponses(plans), pagePlan), HttpStatus.OK);
     }
 }
