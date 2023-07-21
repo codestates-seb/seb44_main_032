c1c2fa2147e05e1554cc6e9d32260a72a7c0ffe9
gibbeunLee <124775054+gibbeunLee@users.noreply.github.com>
2023-07-21 15:07:26 +0900

    test

--- server/src/test/java/com/codeassembly/server/CodeassembleApplicationTests.java
+++ server/src/test/java/com/codeassembly/server/CodeassembleApplicationTests.java
@@ -1,13 +1,13 @@
-package com.codeassembly.server;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CodeassembleApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.codeassembly.server;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class CodeassembleApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
