12adac59e9707f977ae2bcccf7f930b11a721c93
YoonE_ <wldbseja@daum.net>
2023-07-21 17:11:39 +0900

    feat : 메인 페이지 1차 테스트 커밋

--- client/src/page/Main/Main.tsx
+++ client/src/page/Main/Main.tsx
@@ -4,43 +4,15 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import './Main.css';
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Pagination } from 'swiper/modules';
 import LandingPageOne from '../../components/Main/LandingPageOne';
 import LandingPageTwo from '../../components/Main/LandingPageTwo';
 import LandingPageThree from '../../components/Main/LandingPageThree';
 
 function Main() {
-  const progressCircle = useRef<SVGSVGElement | null>(null);
-  const progressContent = useRef<HTMLSpanElement | null>(null);
-
-  const onAutoplayTimeLeft = (s: any, time: number, progress: number) => {
-    if (progressCircle.current) {
-      progressCircle.current.style.setProperty(
-        '--progress',
-        String(1 - progress),
-      );
-    }
-    if (progressContent.current) {
-      progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
-    }
-  };
-
   return (
     <>
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Autoplay, Pagination, Navigation]}
-        onAutoplayTimeLeft={onAutoplayTimeLeft}
-        className="mySwiper"
-      >
+      <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
         <SwiperSlide>
           <LandingPageOne />
         </SwiperSlide>
@@ -50,12 +22,6 @@ function Main() {
         <SwiperSlide>
           <LandingPageThree />
         </SwiperSlide>
-        <div className="autoplay-progress" slot="container-end">
-          <svg viewBox="0 0 48 48" ref={progressCircle}>
-            <circle cx="24" cy="24" r="20"></circle>
-          </svg>
-          <span ref={progressContent}></span>
-        </div>
       </Swiper>
     </>
   );
